fbbfa97c4083537bcdc90e4f0ec5045501f5021e
seno-b <bangshinho@gmail.com>
2021-11-05 11:23:36 +0900

    feat: 3. 스프링 MVC 활용 핸들러 메소드 9부 멀티 폼 서브밋

--- src/main/java/me/senob/springwebmvc/SampleController.java
+++ src/main/java/me/senob/springwebmvc/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -14,20 +15,36 @@ import java.util.List;
 @SessionAttributes(value = {"event"}) // model attribute 에 value 즉 'event' 라는 이름이 존재하면 HttpSession에 등록?저장 된다.
 public class SampleController {
 
-    @GetMapping("/events/form")
-    public String eventsForm(Model model, HttpSession httpSession) {
-        Event attributeValue = new Event();
-        attributeValue.setName("test");
-        attributeValue.setLimit(50);
-        model.addAttribute("event", attributeValue);
-        return "/events/form";
+    @GetMapping("/events/form/name")
+    public String eventsFormName(Model model) {
+        model.addAttribute("event", new Event());
+        return "/events/form-name";
     }
 
-    @PostMapping("/events")
-    public String createEvents(@Valid @ModelAttribute Event event, BindingResult bindingResult, Model model) {
+    @PostMapping("/events/form/name")
+    public String eventsFormNameSubmit(@Valid @ModelAttribute Event event, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/events/form";
+            return "/events/form-name";
         }
+
+        return "redirect:/events/form/limit";
+    }
+
+    @GetMapping("/events/form/limit")
+    public String eventsFormLimit(@ModelAttribute Event event, Model model, HttpSession httpSession) {
+
+        Event event1 = (Event) httpSession.getAttribute("event");
+
+        model.addAttribute("event", event);
+        return "/events/form-limit";
+    }
+
+    @PostMapping("/events/form/limit")
+    public String eventsFormLimitSubmit(@Valid @ModelAttribute Event event, BindingResult bindingResult, SessionStatus sessionStatus) {
+        if (bindingResult.hasErrors()) {
+            return "/events/form-limit";
+        }
+        sessionStatus.setComplete();
         return "redirect:/events/list";
     }
 
