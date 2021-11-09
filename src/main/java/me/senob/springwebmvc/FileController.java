9a4db4740db99654473fc3231e676572b419508b
seno-b <bangshinho@gmail.com>
2021-11-10 00:22:28 +0900

    feat: 3. 스프링 MVC 활용 핸들러 메소드 14부 ResponseEntity

--- src/main/java/me/senob/springwebmvc/FileController.java
+++ src/main/java/me/senob/springwebmvc/FileController.java
@@ -1,24 +1,50 @@
 package me.senob.springwebmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
 
-    @GetMapping("files")
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping("/files")
     public String fileUploadForm() {
         return "files/index";
     }
 
-    @PostMapping("files")
+    @PostMapping("/files")
     public String fileUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
         String message = file.getOriginalFilename() + "is uploaded";
         redirectAttributes.addFlashAttribute("message", message);
         return "redirect:/files";
+    }
+
+    @GetMapping("/files/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + fileName);
+
+        File file = resource.getFile();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                .body(resource);
     }
 }
