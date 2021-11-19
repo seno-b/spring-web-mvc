82dc7f1688f16562b1c11afe285e90ed5e4182f8
seno-b <bangshinho@gmail.com>
2021-11-19 21:47:23 +0900

    feat: 3. 스프링 MVC 활용 데이터 바인더 @InitBinder

--- src/main/java/me/senob/springwebmvc/Event.java
+++ src/main/java/me/senob/springwebmvc/Event.java
@@ -1,7 +1,10 @@
 package me.senob.springwebmvc;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 
 public class Event {
 
@@ -12,6 +15,10 @@ public class Event {
 
     @Min(0)
     private Integer limit;
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate startDate;
 
     public Integer getId() {
         return id;
@@ -35,5 +42,13 @@ public class Event {
 
     public void setLimit(Integer limit) {
         this.limit = limit;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
     }
 }
--- src/main/java/me/senob/springwebmvc/EventController.java
+++ src/main/java/me/senob/springwebmvc/EventController.java
@@ -3,6 +3,7 @@ package me.senob.springwebmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -21,6 +22,12 @@ public class EventController {
 //    public void categories(Model model) {
 //        model.addAttribute("categories", List.of("study", "book", "hobby"));
 //    }
+
+    @InitBinder
+    public void InitEventBinder(WebDataBinder webDataBinder) {
+        webDataBinder.setDisallowedFields("id");
+        webDataBinder.addValidators(new EventValidator());
+    }
 
     @ModelAttribute("categories")
     public List<String> categories(){
